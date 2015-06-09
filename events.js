f2f8ea3bb0c591644962878f8e16dd6cb976ed91
Tianxiang Chen <txchendev@gmail.com>
2015-06-09 11:16:05 -0700

    simplify the basic auth of api

--- events.js
+++ events.js
@@ -3,55 +3,55 @@
 import express from 'express'
 import wga from 'wga'
 import bodyParser from 'body-parser'
-import passportHttp from 'passport-http'
-import passport from 'passport'
+import basicAuth from 'basic-auth'
 import eventStore from './event-store'
 
 let api_user = process.env.API_USER
-let api_pass = process.env.API_PASS
-
-if (!api_user || !api_pass) {
-  throw new Error('must define api_user and api_pass in environment')
+if (!api_user) {
+  throw new Error('must define api_user in environment')
 }
-
-passport.use(new passportHttp.BasicStrategy(
-  (user, pass, done) => {
-    if (user === api_user && pass === api_pass) {
-      return done(null, user)
-    } else {
-      return done(null, false)
-    }
-  }
-))
 
 let router = express.Router()
 
-// parse json or urlencoded
+router.use((req, res, next) => {
+  function unauthorized(res) {
+    res.set('WWW-Authenticate', 'Basic realm=Authorization Required')
+    return res.sendStatus(401)
+  }
+  let user = basicAuth(req)
+  if (!user || !user.name) {
+    return unauthorized(res)
+  }
+
+  if (user.name === api_user) {
+    return next()
+  } else {
+    return unauthorized(res)
+  }
+})
+
+// parse body as json or urlencoded
 router.use(bodyParser.json())
 router.use(bodyParser.urlencoded({ extended: true }))
 
-// TODO: use same auth check for entire app, jwt or basic
-// basic auth to protect api
-router.use(passport.authenticate('basic', { session: false }))
-
 // TODO: pagination support
 router.get('/', wga(async (req, res) => {
-    res.json(await eventStore.getEvents())
-  }))
+  res.json(await eventStore.getEvents())
+}))
 
 router.post('/', wga(async (req, res) => {
-    // event - host:string, message:string, msgType:string, other attributes
-    let message = req.body
-    if (!message.hasOwnProperty('host')) {
-      res.status(400).send('must have host information');
-    }
+  // event - host:string, message:string, msgType:string, other attributes
+  let message = req.body
+  if (!message.hasOwnProperty('host')) {
+    res.status(400).send('must have host information');
+  }
 
-    if (!message.hasOwnProperty('msgType')) {
-      message.msgType = 'default'
-    }
+  if (!message.hasOwnProperty('msgType')) {
+    message.msgType = 'default'
+  }
 
-    let result = await eventStore.addEvent(message)
-    res.json(result)
-  }))
+  let result = await eventStore.addEvent(message)
+  res.json(result)
+}))
 
 export default router
