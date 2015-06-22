d40901272dd9e247133461dd801e49f6d588f280
Tianxiang Chen <txchendev@gmail.com>
2015-06-22 13:00:36 -0700

    fix x-forwarded-proto for azure website

--- app.js
+++ app.js
@@ -11,12 +11,20 @@ let app = express()
 app.set('trust proxy', true) // set trust proxy, otherwise req.protocol is not precise
 app.use(logger('dev'))
 
+// Azure doesn't support X-Forwarded-Proto so we add it manually
+app.use((req, res, next) => {
+    if (req.headers['x-arr-ssl'] && !req.headers['x-forwarded-proto']) {
+        req.headers['x-forwarded-proto'] = 'https'
+    }
+    return next()
+})
+
 app.use('/', express.static('static'))
 app.use('/auth', googleauth)
 app.use('/events', events)
 
 // error handling, should be after normal middleware
-app.use(function(err, req, res, next) {
+app.use((err, req, res, next) => {
   if (err.status) {
     res.status(err.status).send(err.statusText)
   } else {
